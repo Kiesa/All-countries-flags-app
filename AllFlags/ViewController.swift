ff594aff932e809c1d86e02ff9fa403654cdb6f6
mac os <macos@macbook-pro-mac.local>
2016-06-17 23:51:57 +0700

    Initial Commit

---
+++ AllFlags/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AllFlags
+//
+//  Created by mac os on 17.06.16.
+//  Copyright © 2016 Khrystenko Dmytro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
